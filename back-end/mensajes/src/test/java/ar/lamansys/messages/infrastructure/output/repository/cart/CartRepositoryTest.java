43153feeeefd6e8601afb64e8f8f27b2da17a4ba
FFernandez3 <florenciafernandez0301@gmail.com>
2024-08-22 19:57:32 -0300

    EC-73 fix test CartRepository

--- back-end/mensajes/src/test/java/ar/lamansys/messages/infrastructure/output/repository/cart/CartRepositoryTest.java
+++ back-end/mensajes/src/test/java/ar/lamansys/messages/infrastructure/output/repository/cart/CartRepositoryTest.java
@@ -1,6 +1,5 @@
 package ar.lamansys.messages.infrastructure.output.repository.cart;
 
-import ar.lamansys.messages.domain.cart.CartStoredBo;
 import ar.lamansys.messages.infrastructure.output.entity.Cart;
 import ar.lamansys.messages.infrastructure.output.repository.CartRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,105 +7,111 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest(showSql = false)
 @EnableJpaRepositories(basePackages = {"ar.lamansys.messages.infrastructure.output"})
 @EntityScan(basePackages = {"ar.lamansys.messages.infrastructure.output"})
 public class CartRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Autowired
     private CartRepository cartRepository;
+
     private Cart testCart;
 
     @BeforeEach
     void setUp() {
-        testCart = new Cart(1, "user123", 1000, false, "seller123");
-        cartRepository.save(testCart);
+        testCart = newCart("user123", 1000, false, "seller123");
+        entityManager.persistAndFlush(testCart);
     }
 
     @Test
     void findByAppUserIdAndSellerId_whenCartExists_shouldReturnCart() {
-        // Act
-        Optional<Cart> result = cartRepository.findByAppUserIdAndSellerId("user123", "seller123");
-
-        // Assert
-        assertThat(result).isPresent();
-        assertThat(result.get().getAppUserId()).isEqualTo("user123");
-        assertThat(result.get().getSellerId()).isEqualTo("seller123");
+        //act & assert
+        assertThat(
+                cartRepository.findByAppUserIdAndSellerId("user123", "seller123")
+        ).isPresent().get().satisfies(cart -> {
+            assertThat(cart.getAppUserId()).isEqualTo("user123");
+            assertThat(cart.getSellerId()).isEqualTo("seller123");
+        });
     }
 
     @Test
     void getCartExists_whenCartExists_shouldReturnCartStoredBo() {
-
-        // Act
-        CartStoredBo result = cartRepository.getCartExists("user123", "seller123");
-
-        // Assert
-        assertThat(result).isNotNull();
-        assertThat(result.getId()).isEqualTo(1);
-        assertThat(result.getAppUserId()).isEqualTo("user123");
-        assertThat(result.getTotalPrice()).isEqualTo(1000);
-        assertThat(result.getIsFinalized()).isFalse();
-        assertThat(result.getSellerId()).isEqualTo("seller123");
+        //act and assert
+        assertThat(
+                cartRepository.getCartExists("user123", "seller123")
+        ).satisfies(cartStoredBo -> {
+            assertThat(cartStoredBo.getId()).isEqualTo(testCart.getId());
+            assertThat(cartStoredBo.getAppUserId()).isEqualTo("user123");
+            assertThat(cartStoredBo.getTotalPrice()).isEqualTo(1000);
+            assertThat(cartStoredBo.getIsFinalized()).isFalse();
+            assertThat(cartStoredBo.getSellerId()).isEqualTo("seller123");
+        });
     }
 
     @Test
     void findByIdAndAppUserId_whenCartExists_shouldReturnCart() {
-
-        // Act
-        Optional<Cart> result = cartRepository.findByIdAndAppUserId(1, "user123");
-
-        // Assert
-        assertThat(result).isPresent();
-        assertThat(result.get().getId()).isEqualTo(1);
-        assertThat(result.get().getAppUserId()).isEqualTo("user123");
+        //act & Assert
+        assertThat(
+                cartRepository.findByIdAndAppUserId(testCart.getId(), "user123")
+        ).isPresent().get().satisfies(cart -> {
+            assertThat(cart.getId()).isEqualTo(testCart.getId());
+            assertThat(cart.getAppUserId()).isEqualTo("user123");
+        });
     }
 
     @Test
     void updateTotalPrice_whenCartExists_shouldUpdateTotalPrice() {
+        //act
+        cartRepository.updateTotalPrice(testCart.getId(), 2000);
 
-        // Act
-        cartRepository.updateTotalPrice(1, 2000);
-
-        // Assert
-        Integer updatedTotalPrice = cartRepository.getTotalPrice(1);
-        assertThat(updatedTotalPrice).isEqualTo(2000);
+        //assert
+        assertThat(
+                cartRepository.getTotalPrice(testCart.getId())
+        ).isEqualTo(2000);
     }
 
     @Test
     void getTotalPrice_whenCartExists_shouldReturnTotalPrice() {
-        // Act
-        Integer result = cartRepository.getTotalPrice(1);
-
-        // Assert
-        assertThat(result).isEqualTo(1000);
+        //act and assert
+        assertThat(
+                cartRepository.getTotalPrice(testCart.getId())
+        ).isEqualTo(1000);
     }
 
     @Test
     void getSellerById_whenCartExists_shouldReturnSellerId() {
-        // Act
-        String result = cartRepository.getSellerById(1);
-        // Assert
-        assertThat(result).isEqualTo("seller123");
+        //Act and assert
+        assertThat(
+                cartRepository.getSellerById(testCart.getId())
+        ).isEqualTo("seller123");
     }
 
     @Test
     void getIsFinalizedById_whenCartExists_shouldReturnIsFinalized() {
-        // Act
-        boolean result = cartRepository.getIsFinalizedById(1);
-        // Assert
-        assertThat(result).isFalse();
+        //act and assert
+        assertThat(
+                cartRepository.getIsFinalizedById(testCart.getId())
+        ).isFalse();
     }
 
     @Test
     void updateIsFinalized_whenCartExists_shouldUpdateIsFinalized() {
-        // Act
-        cartRepository.updateIsFinalized(1);
-        // Assert
-        boolean updatedIsFinalized = cartRepository.getIsFinalizedById(1);
-        assertThat(updatedIsFinalized).isTrue();
+        //act
+        cartRepository.updateIsFinalized(testCart.getId());
+
+        //assert
+        assertThat(
+                cartRepository.getIsFinalizedById(testCart.getId())
+        ).isTrue();
+    }
+
+    private static Cart newCart(String appUserId, int totalPrice, boolean isFinalized, String sellerId) {
+        return new Cart(null, appUserId, totalPrice, isFinalized, sellerId);
     }
 }
