c35e86e4699e5343aa3e9876f51f98f4887cd8be
Pia Bedini <pbedinicrocci@gmail.com>
2024-08-29 17:12:57 -0300

    EC-90 fix equals en AssertStockChanged

--- back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/AssertStockChanged.java
+++ back-end/mensajes/src/main/java/ar/lamansys/messages/application/product/AssertStockChanged.java
@@ -5,13 +5,15 @@ import ar.lamansys.messages.application.product.port.ProductStorage;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @Service
 public class AssertStockChanged {
     private final ProductStorage productStorage;
 
     public void run(Integer productId, Integer newStock) throws StockHasNotChangedException {
-        if (productStorage.getStock(productId) == newStock) {
+        if (Objects.equals(productStorage.getStock(productId), newStock)) {
             throw new StockHasNotChangedException(productId,newStock);
         }
     }
